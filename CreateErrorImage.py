2e3cf2a7738ea5aa61d2c15bdd2edf549be23716
egentry <gentry.e@gmail.com>
2013-08-13 12:57:08 -0400

    Added PSF and error scripts

---
+++ CreateErrorImage.py
@@ -0,0 +1,114 @@
+def main():
+    """
+    Takes in a drizzled fits image (final weight type "EXP") and creates a Poisson photon-noise map. 
+
+
+    Things to do later:
+        Check about taking into account gain
+
+        In units of COUNTS or COUNTS/SEC?
+            How does a sci_drz image report data in terms of counts?
+                Normalized to nominal exposure time?
+
+        Read in gain, exposure time (don't leave as hardcoded)
+
+   
+        
+
+
+    Version history
+        2013.08.12 - Eric Gentry - Began writing script
+        2013.08.13 - Eric Gentry - initial draft completed
+    """
+
+    from astropy.io import fits
+    import numpy as np
+    import os
+
+###
+
+    main_dir = '/home/egentry/Data/HST/calib_attempt3/'
+
+    processed_file_dir = 'Processed_Files/'
+    
+
+    if not os.path.exists( main_dir + processed_file_dir):
+        os.makedirs(main_dir + processed_file_dir)
+
+    sci_filename = 'f160w_drz_sci.fits'
+    wht_filename = 'f160w_drz_wht.fits'
+    ctx_filename = 'f160w_drz_ctx.fits'
+
+
+###
+
+    exp_time = 2708.81 # seconds
+
+    sci_hdulist = fits.open(main_dir + sci_filename)
+    wht_hdulist = fits.open(main_dir + wht_filename)
+    ctx_hdulist = fits.open(main_dir + ctx_filename)
+
+    sci_data = sci_hdulist[0].data
+    wht_data = wht_hdulist[0].data
+    ctx_data = ctx_hdulist[0].data
+
+    sci_size_x = sci_data.shape[1]
+    sci_size_y = sci_data.shape[0]
+
+    wht_size_x = wht_data.shape[1]
+    wht_size_y = wht_data.shape[0]
+
+    ctx_size_x = ctx_data.shape[1]
+    ctx_size_y = ctx_data.shape[0]
+
+    if ( sci_size_x != wht_size_x ) or ( sci_size_y != wht_size_y):
+        print 'SCI and WHT dimensions do not match'
+        print 'SCI Dimensions: ', sci_size_x, ' by ' , sci_size_y
+        print 'SCI Dimensions: ', wht_size_x, ' by ' , wht_size_y
+        return
+
+    if ( sci_size_x != ctx_size_x ) or ( sci_size_y != ctx_size_y):
+        print 'SCI and CTX dimensions do not match'
+        print 'SCI Dimensions: ', sci_size_x, ' by ' , sci_size_y
+        print 'CTX Dimensions: ', ctx_size_x, ' by ' , ctx_size_y
+        return
+
+    error_data      = np.empty( sci_data.shape )
+    bad_pixel_data  = np.zeros( sci_data.shape )
+
+    for x_i in xrange(sci_size_x):
+        for y_i in xrange(sci_size_y):
+            if ctx_data[y_i][x_i] == 0:
+                bad_pixel_data[y_i][x_i] = 1
+
+            if np.isnan(sci_data[y_i][x_i]):
+                sci_data[y_i][x_i] = 0
+                bad_pixel_data[y_i][x_i] = 1
+            
+            if np.isinf(sci_data[y_i][x_i]):
+                bad_pixel_data[y_i][x_i] = 1
+                sci_data[y_i][x_i] = 0
+
+            if wht_data[y_i][x_i] == 0:
+                bad_pixel_data[y_i][x_i] = 1 
+                wht_data[y_i][x_i] = 1       
+    
+
+    error_data = sci_data * wht_data
+
+    error_data = np.absolute(error_data) ** .5
+    
+    error_data = np.divide(error_data, wht_data)  # elementwise
+
+
+
+    bad_pixel_hdulist = fits.PrimaryHDU(bad_pixel_data)
+    error_hdulist     = fits.PrimaryHDU(error_data)
+
+
+    bad_pixel_hdulist.writeto(main_dir + processed_file_dir + 'bad_pixel.fits', clobber=True)
+    error_hdulist.writeto(main_dir + processed_file_dir + '/error.fits', clobber=True)
+
+
+    
+
---
+++ DrizzlePSFs.py
@@ -0,0 +1,145 @@
+main_dir = '/home/egentry/Data/HST/PictorA/PSFs/drizzler/'
+
+artificial_star_dir = 'artificial_stars/'
+
+
+from astropy.io import fits
+import os
+
+
+def main():
+    """ Prepare PSFs to be drizzled 
+
+        Expects FLT + PSF fits files
+            Number of FLT files should match number of PSF files
+            FLT files should be multiextension .fits files, as given by NASA ('SCI' = index 1)
+
+        Pixels here follow a zero indexing convention. Be careful -- DS9 uses 1 indexing!
+
+        Currently this is only meant for a very specific purpose. Many values are hardcoded. This may be fixed later.
+
+
+        Version History
+            2013.08.09 -- Eric Gentry -- Created limited purpose draft
+
+    """
+
+    if not os.path.exists(main_dir + artificial_star_dir):
+        os.makedirs(main_dir + artificial_star_dir)
+
+
+
+    #0-indexed:
+    agn_centers = [ (363, 143), (366, 146) , (369, 150)]
+
+    psf_size = 276
+    psf_center = 138
+
+
+    for Image_index in xrange(3):
+
+        #0 indexed:
+        min_nonzero_x = agn_centers[Image_index][0] - psf_center
+        max_nonzero_x = agn_centers[Image_index][0] + psf_center -1
+        min_nonzero_y = agn_centers[Image_index][1] - psf_center
+        max_nonzero_y = agn_centers[Image_index][1] + psf_center -1
+
+    
+        psf_filename =         'psf_{}.fits'.format(Image_index)
+        flt_filename = 'ibjx01y{}q_flt.fits'.format(Image_index)
+        
+        psf_hdulist = fits.open(main_dir + psf_filename)
+        
+        flt_hdulist = fits.open(main_dir + flt_filename)
+
+        
+        flt_data = flt_hdulist[1].data
+        psf_data = psf_hdulist[0].data
+
+        flt_size_x = flt_data.shape[1]
+        flt_size_y = flt_data.shape[0]
+
+        psf_size_x = psf_data.shape[1]
+        psf_size_y = psf_data.shape[0]
+
+        
+        flt_data.fill(0)
+
+#        for x_i in xrange(flt_size_x):
+#            for y_i in xrange(flt_size_y):
+#                flt_data[y_i][x_i] =  0
+
+        for x_i in xrange(psf_size_x):
+            for y_i in xrange(psf_size_y):
+                flt_data[y_i + min_nonzero_y][x_i + min_nonzero_x] = psf_data[y_i][x_i] * 1e6
+
+        flt_hdulist.writeto(main_dir + artificial_star_dir + flt_filename, clobber=True)
+        flt_hdulist.close()
+        psf_hdulist.close()
+
+def drizzle():
+    """ 
+        FUNCTION NOT YET TESTED
+    """
+    from drizzlepac import astrodrizzle
+
+    
+
+    astrodrizzle.AstroDrizzle('*flt.fits', output='psf', static=False, skysub=False,
+        driz_separate=False, median=False, blot=False, driz_cr=False, 
+        driz_combine=True, final_wht_type='EXP', final_pixfrac=0.7, final_bits=4096,
+        final_wcs=True, final_scale=0.06666)
+                        
+
+
+def crop():
+    """
+        Takes a drizzled TinyTim PSF, filters out NaN values, crops to 30arcsec box suitable for GALFIT
+    """
+    
+    import numpy as np
+
+    psf_rootname = 'psf'
+
+    psf_filename = psf_rootname + '_drz_sci.fits'
+
+    psf_hdulist = fits.open(main_dir + artificial_star_dir + psf_filename)
+
+    psf_data = psf_hdulist[0].data
+
+    psf_size_x = psf_data.shape[1]
+    psf_size_y = psf_data.shape[0]
+
+    
+    # Filter out bad data -- later check why this happens (maybe final_fillval?)
+#    for x_i in xrange(psf_size_x):
+#        for y_i in xrange(psf_size_y):
+#            if np.isnan(psf_data[y_i][x_i]):
+#                print 'found a nan at:', x_i, y_i
+
+    # Filter out bad data -- later check why this happens (maybe final_fillval?)
+    #Set all NaN's to 0 (Also changes +/- Inf, but that shouldn't affect this data)
+    psf_data = np.nan_to_num(psf_data)
+
+    valid_coords =  np.nonzero(psf_data)
+
+
+    min_nonzero_x = np.amin( valid_coords[1] )
+    max_nonzero_x = np.amax( valid_coords[1] )
+    min_nonzero_y = np.amin( valid_coords[0] )
+    max_nonzero_y = np.amax( valid_coords[0] )
+
+    psf_data_cropped = psf_data[min_nonzero_y : max_nonzero_y + 1, min_nonzero_x : max_nonzero_x + 1] * 1e-6
+
+    psf_cropped_hdulist = fits.PrimaryHDU(psf_data_cropped)
+    psf_cropped_hdulist.writeto(main_dir + artificial_star_dir + 'psf_drz_sci_cropped.fits', clobber=True)
+    
+    psf_hdulist.close()    
+
+
+
+    
+    
+
+
+    
